ddb0f729fec289825227303f4ccf26412da5483a
clr-cera <clr-cera@users.noreply.github.com>
2023-11-06 16:42:45 -0300

    starting primes

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate integer_sqrt;
+
 use integer_sqrt::IntegerSquareRoot;
 
 fn transform(a: u64, e: u64, d: u64) -> u64{
@@ -56,34 +57,55 @@ pub fn find_n_and_inverse(divisor: u64) -> (u64, u64){
 
 pub fn inverse(number: u64, divisor: u64) -> u64{
 
-        let mut bigger: u64 = divisor;
-        let mut smaller: u64 = number % divisor;
+    let mut bigger: u64 = divisor;
+    let mut smaller: u64 = number % divisor;
 
-        let mut actualq: i128;
-        let mut lastq: i128 = 1;
-        let mut lastlastq: i128 = 0;
+    let mut actualq: i128;
+    let mut lastq: i128 = 1;
+    let mut lastlastq: i128 = 0;
 
-        loop {
-            let rest: u64 = bigger % smaller;
-            actualq = lastlastq - (lastq * i128::from(bigger / smaller));
+    loop {
+        let rest: u64 = bigger % smaller;
+        actualq = lastlastq - (lastq * i128::from(bigger / smaller));
 
-            match rest { 
-                1 => {
-                    let inverse = ((divisor as i128 + actualq) % divisor as i128) as u64;
-                    if inverse < number {
-                        return ((divisor as i128 + actualq) % divisor as i128) as u64;
-                    }                
-                }
-                
-                0 => break,
-                _ => (),
+        match rest { 
+            1 => {
+                let inverse = ((divisor as i128 + actualq) % divisor as i128) as u64;
+                if inverse < number {
+                    return ((divisor as i128 + actualq) % divisor as i128) as u64;
+                }                
             }
-
-            bigger = smaller;
-            smaller = rest; 
-
-            lastlastq = lastq;
-            lastq = actualq;
+            
+            0 => break,
+            _ => (),
         }
+
+        bigger = smaller;
+        smaller = rest; 
+
+        lastlastq = lastq;
+        lastq = actualq;
+    }
     return 1;
 }
+
+
+pub fn composite_test_fermat(number: u128, test: u128) -> bool{
+    let fermat_result: u128 = power_mod(test, number-1, number);
+
+    if fermat_result != 1 {return true;}
+    else {return false;}
+}
+
+pub fn composite_test_miller_rabin(number: u128, basis: u128) -> bool{
+
+}
+
+fn power_mod(number: u128, exp: u128, module: u128) -> u128 {
+    let mut result: u128 = 1;
+    for _ in 1..=exp{
+        result = (result * number) % module;
+    }
+
+    return result;
+} 
