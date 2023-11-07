46f0cc4ad2c95643c70a0fe969db49c3f27d1101
clr-cera <clrcera05@gmail.com>
2023-11-07 19:31:37 -0300

    lucas primality test is not done yet but jacobi symbol is already constructed

--- src/primality/tests.rs
+++ src/primality/tests.rs
@@ -30,11 +30,69 @@ pub fn composite_test_miller_rabin(number: u128, base: u128) -> bool{
 
 /// This function checks if a number is prime using miller-rabin test of composites, if it returns
 /// true, then the informed number has 1/4^10 of probability of being a prime
-pub fn is_prime(number: u128) -> bool {
-    if number % 2 == 0 {return false;}
+pub fn is_prime_miller(number: u128, base: u128) -> bool {
+    if number == 0 || number == 1 || number % 2 == 0 { 
+        return false;
+    }
 
-    for i in 2..=11{
-        if composite_test_miller_rabin(number, i) == true {
+    if composite_test_miller_rabin(number, base) == true {
+        return false;
+    }
+
+    return true;
+}
+
+pub fn is_prime_fermat(number: u128, base: u128) -> bool {
+    if number == 0 || number == 1 || number % 2 == 0 { 
+        return false;
+    }
+
+    if composite_test_fermat(number, base) == true {
+        return false;
+    }
+
+
+    return true;
+}
+
+pub fn is_prime_baillie(number: u128) -> bool {
+    if number == 0 || number == 1 || number % 2 == 0 { 
+        return false;
+    }
+
+    if composite_test_miller_rabin(number, 2) == true {
+        return false;
+    }
+
+    let mut d: i128 = 5;
+
+    while jacobi(d, number as i128) != -1{
+        if d < 0 {
+            d = (d-2) * -1;
+        }
+        else {
+            d = (d+2) * -1;
+        }
+    }
+    
+    let p = 1;
+    let q = (1 - d)/4;
+
+    if composite_test_lucas(number, d, p, q) == true {
+        return false;
+    }
+
+
+    return true;
+}
+
+pub fn is_prime(number: u128) -> bool {
+    if number == 0 || number == 1 || number % 2 == 0 { 
+        return false;
+    }
+    
+    for base in 2..10 {
+        if composite_test_miller_rabin(number, base) == true {
             return false;
         }
     }
@@ -75,6 +133,12 @@ pub fn composite_test_bruteforce(number: u128) -> bool{
     return false;
 } 
 
+/// This function checks if a number is a composite using lucas probable prime test
+pub fn composite_test_lucas(number: u128, d: i128, p: i128, q: i128) -> bool {
+
+    return true;
+}
+
 /// This function decomposes a number into number = 2^exp + q. It returns (exp, q).
 fn miller_decompose(mut number: u128) -> (u16, u128){
     if number % 2 == 0 {return (0, 0);}
@@ -91,4 +155,33 @@ fn miller_decompose(mut number: u128) -> (u16, u128){
     (exp, number)
 }
 
+/// This function calculates the jacobi symbol of a and n.
+fn jacobi(mut a: i128, mut n:i128) -> i16 {
+    a = a % n;
+    
+    let mut t = 1;
+    let mut r;
 
+    while a != 0 {
+        while a % 2 == 0 {
+            a /= 2;
+            r = n % 8;
+            
+            if r == 3 || r == 5{
+                t = -t;
+            }
+        }
+
+        r = n;
+        n =a;
+        a = r;
+        
+        if a % 4 == 3 && n % 4 == 3 {
+            t = -t;
+        }
+        a = a % n;
+    }
+
+    if n == 1 {return t;} 
+    else {return 0}
+}
